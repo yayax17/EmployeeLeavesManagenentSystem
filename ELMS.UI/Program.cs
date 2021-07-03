758f901fcfa4251cc1ec5d0334b89f4842f38cfd
Mark Anthony Acorda <82651591+yayax17@users.noreply.github.com>
2021-07-03 12:38:31 +0800

    Add files via upload

---
+++ ELMS.UI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ELMS.BL;
+
+namespace ELMS.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.WriteLine("\nWELCOME TO EMPLOYEE MANAGEMENT SYSTEM\n");
+            Console.WriteLine("     Log in to continue   \n\n");
+            Console.Write("Enter Username: ");
+            string admin = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            string pass = Console.ReadLine();
+
+            if (admin == "login" && pass == "password")
+            {
+                Console.WriteLine("\nLogin Successfully.\n");
+                User.DisplayHome();
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid login. Please Try Again.\n");
+            }
+        }
+    }
+}
